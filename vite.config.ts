fa0dd4fb604752102fa8203e03881521dbeec434
BJJIN <17856419249@163.com>
2022-04-07 07:54:02 +0800

    delete vite eslint plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteEslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteEslint(),],
+  plugins: [react()],
   css: {
     modules: {
       generateScopedName: '[name]__[local]__[hash:base64:5]',
